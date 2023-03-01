806d4df03f512665903acff185439ded053c6aaa
Oriol <bover73@gmail.com>
2023-03-01 21:45:07 +0100

    finish first iteration game

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const TURNS = {
   x: 'X',
@@ -36,26 +36,51 @@ function App () {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.x)
   const [winner, setWinner] = useState(null)
-
-  useEffect(() => {
-    setWinner(checkWinner(board))
-  }, [board])
+  const [draw, setDraw] = useState(false)
 
   const handleClick = (index) => {
-    if (winner) return
+    // if there is a winner or the square is already filled, do nothing
+    if (board[index] || winner) return
+
+    // create a new board array and update the square with the current turn
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
     setTurn(turn === TURNS.x ? TURNS.o : TURNS.x)
+
+    // check for a winner
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+      // check for a draw
+    } else if (newBoard.every((square) => square !== null)) {
+      setDraw(true)
+    }
+  }
+
+  // reset the game
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.x)
+    setWinner(null)
+    setDraw(false)
   }
 
   return (
     <>
-      <h1>Tic Tac Toe</h1>
-      {winner && <h2>{winner} wins!</h2>}
-      <div className='board'>
-        {board.map((square, index) => (<Square key={index} value={square} onSquareClick={() => handleClick(index)} />))}
-      </div>
+      <header>
+        <h1>Tic Tac Toe</h1>
+      </header>
+      <main>
+        <section className='results'>
+          {winner && <h2>{winner} wins!</h2>}
+          {draw && <h2>Draw!</h2>}
+          {winner || draw ? <button onClick={resetGame}>Play again</button> : <h2>Turn: {turn}</h2>}
+        </section>
+        <section className='board'>
+          {board.map((square, index) => (<Square key={index} value={square} onSquareClick={() => handleClick(index)} />))}
+        </section>
+      </main>
     </>
   )
 }
