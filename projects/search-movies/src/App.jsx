e8de08ca772fcc5084f26afabf7096f4bc3021b9
Oriol <bover73@gmail.com>
2023-03-04 19:00:43 +0100

    first movies layout

--- projects/search-movies/src/App.jsx
+++ projects/search-movies/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
+import response from './mocks/search-response.json'
+// const API_KEY = import.meta.env.VITE_API_KEY
 
 function App () {
   return (
     <>
-      <h1>hello world</h1>
+      <h1>Search Movies</h1>
+      <form className='search-form'>
+        <input type='text' placeholder='Search for a movie...' />
+        <button>Search</button>
+      </form>
+      <section className='movies'>
+        {response.Search.map(movie => (
+          <article className='movie' key={movie.imdbID}>
+            <img src={movie.Poster} alt={movie.Title} />
+            <div className='movie-info'>
+              <h2>{movie.Title}</h2>
+              <time>{movie.Year}</time>
+            </div>
+          </article>))}
+      </section>
     </>
   )
 }
