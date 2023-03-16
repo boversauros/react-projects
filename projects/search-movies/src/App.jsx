33b1bbb09b9328e614988737cf9561ea4fdd45f9
Oriol <bover73@gmail.com>
2023-03-16 21:35:09 +0100

    remove single response mock and modal

--- projects/search-movies/src/App.jsx
+++ projects/search-movies/src/App.jsx
@@ -1,50 +1,7 @@
 import './App.css'
 import response from './mocks/search-response.json'
-import singleResponse from './mocks/single-response.json'
 
 // const API_KEY = import.meta.env.VITE_API_KEY
-function ModalMovie () {
-  return (
-    <div className='modal'>
-      <div className='modal-content'>
-        <div className='modal-header'>
-          <h2>{singleResponse.Title}</h2>
-          <button className='close-modal-btn'>X</button>
-        </div>
-        <div className='modal-body'>
-          <img src={singleResponse.Poster} alt={singleResponse.Title} />
-          <div className='movie-info'>
-            <p>{singleResponse.Plot}</p>
-            <ul>
-              <li>
-                <strong>Genre:</strong> {singleResponse.Genre}
-              </li>
-              <li>
-                <strong>Released:</strong> {singleResponse.Released}
-              </li>
-              <li>
-                <strong>Rated:</strong> {singleResponse.Rated}
-              </li>
-              <li>
-                <strong>IMDB Rating:</strong> {singleResponse.imdbRating}
-              </li>
-              <li>
-                <strong>Director:</strong> {singleResponse.Director}
-              </li>
-              <li>
-                <strong>Writer:</strong> {singleResponse.Writer}
-              </li>
-              <li>
-                <strong>Actors:</strong> {singleResponse.Actors}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 function App () {
   return (
     <>
@@ -64,7 +21,6 @@ function App () {
             <button className='movie-btn'>More Info</button>
           </article>))}
       </section>
-      <ModalMovie />
     </>
   )
 }
