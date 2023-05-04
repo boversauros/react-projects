90ed0b04b3631b3f5f910b8b8342459804c5482a
Oriol Bover <bover73@gmail.com>
2023-05-04 18:29:19 +0200

    init appointment project

---
+++ appointment-rescheduling/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="container mx-auto px-4 pt-32">
+      <header>
+        <h1 className="text-3xl font-light">
+          Confirm your appointment with{" "}
+          <span className="font-bold">Dr. Simeon Molas</span>
+        </h1>
+      </header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
