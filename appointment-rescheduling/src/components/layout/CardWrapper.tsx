d1d629a80dd58370ee6d5b06b630e0e03a469b40
Oriol <bover73@gmail.com>
2023-05-05 14:59:29 +0200

    remove project from root

--- appointment-rescheduling/src/components/layout/CardWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, ReactNode } from "react";
-
-export const CardWrapper: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <article className="bg-white rounded-md shadow-sm">{children}</article>
-  );
-};
